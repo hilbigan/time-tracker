13d94d9b526d14647c43fe52e7318e7858f38e1d
Aaron Hilbig <darkolan2@gmail.com>
2021-06-27 17:28:08 +0200

    Refactor: Move UI stuff into UI struct

--- src/main.rs
+++ src/main.rs
@@ -34,40 +34,49 @@ fn get_input() -> Option<usize> {
     Some(number)
 }
 
+fn get_filename_today() -> String {
+    let time = Local::now() - Duration::hours((*DAY_START / 2) as i64);
+    get_filename_by_date(time.year() as usize, time.month() as usize, time.day() as usize)
+}
+
+fn get_filename_by_date(year: usize, month: usize, day: usize) -> String {
+    format!("/home/aaron/.local/share/{}-{}-{}.json", year, month, day)
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
-struct Occupation {
+struct Activity {
     name: String,
     productive: bool
 }
 
-impl Occupation {
-    fn get_all() -> Vec<Occupation> {
+impl Activity {
+    fn get_all() -> Vec<Activity> {
         vec![
-            Occupation { name: "Arbeit".to_string(), productive: true },
-            Occupation { name: "Baustelle".to_string(), productive: true },
-            Occupation { name: "Hobby".to_string(), productive: true },
-            Occupation { name: "Pause".to_string(), productive: false },
-            Occupation { name: "Programmieren".to_string(), productive: true },
-            Occupation { name: "Uni".to_string(), productive: true },
-            Occupation { name: "Unterwegs".to_string(), productive: false },
-            Occupation { name: "Zocken".to_string(), productive: false },
+            Activity { name: "Arbeit".to_string(), productive: true },
+            Activity { name: "Baustelle".to_string(), productive: true },
+            Activity { name: "Hobby".to_string(), productive: true },
+            Activity { name: "Pause".to_string(), productive: false },
+            Activity { name: "Programmieren".to_string(), productive: true },
+            Activity { name: "Uni".to_string(), productive: true },
+            Activity { name: "Unterwegs".to_string(), productive: false },
+            Activity { name: "Zocken".to_string(), productive: false },
         ]
     }
     
-    fn get_by_name(occps: &[Occupation], name: &str) -> Option<Self> {
-        occps.iter().find(|o| o.name == name).cloned()
+    fn get_by_name(actis: &[Activity], name: &str) -> Option<Self> {
+        actis.iter().find(|o| o.name == name).cloned()
     }
     
-    fn prompt(occps: &[Occupation]) -> Option<&Occupation> {
-        occps.iter().enumerate().for_each(|(i, o)| {
+    fn prompt(actis: &[Activity]) -> Option<&Activity> {
+        actis.iter().enumerate().for_each(|(i, o)| {
             println!("\t{}: {}", i, o);
         });
         let number = get_input()?;
-        Some(&occps[number])
+        Some(&actis[number])
     }
 }
 
-impl Display for Occupation {
+impl Display for Activity {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         let color_idx = self.name.chars().map(|c| c as usize).sum::<usize>() % COLORS.len();
         f.write_str(&*format!("{}", self.name.color(COLORS[color_idx])))?;
@@ -114,7 +123,7 @@ impl Display for SlotRef {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Day {
-    time_slots: Vec<Option<Occupation>>
+    time_slots: Vec<Option<Activity>>
 }
 
 impl Default for Day {
@@ -124,13 +133,13 @@ impl Default for Day {
 }
 
 impl Day {
-    pub fn entry_before_now(&self) -> Option<(SlotRef, &Occupation)> {
+    pub fn entry_before_now(&self) -> Option<(SlotRef, &Activity)> {
         (*DAY_START .. *SlotRef::now()).rev().into_iter()
             .map(|s| (s, &self.time_slots[s])).find(|it| it.1.is_some())
             .and_then(|(s, o)| Some((SlotRef(s), o.as_ref().unwrap())))
     }
     
-    pub fn slots(&self) -> impl Iterator<Item = (SlotRef, &Option<Occupation>)> {
+    pub fn slots(&self) -> impl Iterator<Item = (SlotRef, &Option<Activity>)> {
         self.time_slots.iter().enumerate().map(|(s, o)| {
             (SlotRef(s), o)
         })
@@ -155,20 +164,93 @@ impl Day {
     pub fn score(&self) -> f32 {
         self.hours_productive() as f32 / 12.
     }
+    
+    fn print_stats(&self, with_current_time: bool, trim_start: bool) {
+        let first_non_empty = self.first_non_empty();
+        self.slots().for_each(|(s, o)| {
+            if (!with_current_time || *s <= *SlotRef::now()) && (!trim_start || first_non_empty.is_none() || *s >= *first_non_empty.unwrap()) {
+                println!("{}-{} - {}", s, s.next(), if let Some(act) = o { act.to_string() } else { "empty".to_string() });
+            }
+        });
+        println!("Hours Productive: {}, Score: {:0.2}", self.hours_productive(), self.score());
+    }
+    
+    fn write(&self, path: &Path) {
+        fs::write(path, serde_json::to_string(&self).unwrap()).expect("write failed");
+    }
 }
 
-fn get_filename() -> String {
-    let time = Local::now() - Duration::hours((*DAY_START / 2) as i64);
-    get_filename_by_date(time.year() as usize, time.month() as usize, time.day() as usize)
+struct UI {
+    day: Day,
+    file: PathBuf,
+    activities: Vec<Activity>
 }
 
-fn get_filename_by_date(year: usize, month: usize, day: usize) -> String {
-    format!("/home/aaron/.local/share/{}-{}-{}.json", year, month, day)
+impl UI {
+    fn ask_about_activity(&mut self, start: SlotRef, end: SlotRef) {
+        println!("What did you do from {} - {}?", start.to_string().yellow(), end.to_string().yellow());
+        let act = Activity::prompt(&self.activities);
+        if let Some(act) = act {
+            for s in *start .. *end {
+                self.day.time_slots[s] = Some(act.clone());
+            }
+        }
+    }
+    
+    fn ask_about_activity_now(&mut self) {
+        let now = *SlotRef::now();
+        let start = self.day.now_or_last_entry();
+        let end = now + 1;
+        self.ask_about_activity(start, SlotRef(end));
+    }
+    
+    fn edit_with_text_editor(&mut self) {
+        let tmp_file = PathBuf::from_str("/tmp/time-track.tmp").unwrap();
+        let mut data = String::new();
+        self.day.slots().for_each(|(s, o)| {
+            writeln!(&mut data, "{}-{} - {}", s, s.next(), if let Some(act) = o { act.name.clone() } else { "empty".to_string() });
+        });
+        fs::write(&tmp_file, data);
+        let exit_code = Command::new(EDITOR)
+            .arg(tmp_file.to_str().unwrap())
+            .status()
+            .expect("could not open editor");
+        if !exit_code.success() {
+            println!("Editor exited with non-zero exit code!");
+        } else {
+            let data = fs::read_to_string(tmp_file).expect("could not read file");
+            self.day.time_slots = data.lines().enumerate().map(|(i, o)| {
+                Activity::get_by_name(&self.activities, &o[14..])
+            }).collect();
+        }
+    }
+    
+    fn split(&mut self) {
+        let now_or_last_entry = self.day.now_or_last_entry();
+        let possible_slots = (*now_or_last_entry + 1 .. *SlotRef::now() + 1).into_iter().collect::<Vec<_>>();
+        if possible_slots.is_empty() {
+            println!("{}", "There's nothing to split!".red());
+            return;
+        }
+        println!("Where to split?");
+        for (i, s) in possible_slots.iter().enumerate() {
+            println!("{}: {}", i, SlotRef(*s));
+        }
+        if let Some(choice) = get_input() {
+            self.ask_about_activity(now_or_last_entry, SlotRef(possible_slots[choice]));
+            self.day.write(self.file.as_path());
+            self.ask_about_activity(SlotRef(possible_slots[choice]), SlotRef::now().next());
+        }
+    }
+    
+    fn save(&self) {
+        self.day.write(&self.file);
+    }
 }
 
 fn main() {
-    let occupations = Occupation::get_all();
-    let file = PathBuf::from(get_filename());
+    let activities = Activity::get_all();
+    let file = PathBuf::from(get_filename_today());
     let mut day = if file.exists() {
         println!("Reading from {:?}", file);
         serde_json::from_str(fs::read_to_string(file.clone()).expect("could not read file").as_str()).unwrap()
@@ -181,12 +263,13 @@ fn main() {
         println!("Recent activity: {} (until {})", entry.1, entry.0.next());
     }
     println!("Current slot: {} ({})", SlotRef::now(),
-             if let Some(occ) = &day.time_slots[now] {
-                 format!("{}", occ)
+             if let Some(act) = &day.time_slots[now] {
+                 format!("{}", act)
              } else {
                  "no activity so far".bold().to_string()
              }
     );
+    let mut ui = UI { day, file, activities };
     if let Some(arg) = std::env::args().nth(1) {
         match arg.as_str() {
             "h" | "help" => {
@@ -209,89 +292,27 @@ fn main() {
                 let day = get_input().unwrap_or(default_day);
                 let file = PathBuf::from(get_filename_by_date(year, month, day));
                 println!("Loading file {:?}", file);
-                let file = serde_json::from_str(fs::read_to_string(file).expect("could not read file").as_str()).unwrap();
-                print_day_stats(&file, false, true);
+                let day: Day = serde_json::from_str(fs::read_to_string(file).expect("could not read file").as_str()).unwrap();
+                day.print_stats(false, true);
             },
             "t" | "today" => {
-                print_day_stats(&day, true, true);
+                ui.day.print_stats(true, true);
             },
             "w" | "week" => {
                 unimplemented!()
             },
             "e" | "edit" => {
-                let tmp_file = PathBuf::from_str("/tmp/time-track.tmp").unwrap();
-                let mut data = String::new();
-                day.slots().for_each(|(s, o)| {
-                    writeln!(&mut data, "{}-{} - {}", s, s.next(), if let Some(occ) = o { occ.name.clone() } else { "empty".to_string() });
-                });
-                fs::write(&tmp_file, data);
-                let exit_code = Command::new(EDITOR)
-                    .arg(tmp_file.to_str().unwrap())
-                    .status()
-                    .expect("could not open editor");
-                if !exit_code.success() {
-                    println!("Editor exited with non-zero exit code!");
-                } else {
-                    let data = fs::read_to_string(tmp_file).expect("could not read file");
-                    day.time_slots = data.lines().enumerate().map(|(i, o)| {
-                        Occupation::get_by_name(&occupations, &o[14..])
-                    }).collect();
-                }
+                ui.edit_with_text_editor();
             },
             "s" | "split" => {
-                let now_or_last_entry = day.now_or_last_entry();
-                let possible_slots = (*now_or_last_entry + 1 .. *SlotRef::now() + 1).into_iter().collect::<Vec<_>>();
-                if possible_slots.is_empty() {
-                    println!("{}", "There's nothing to split!".red());
-                    return;
-                }
-                println!("Where to split?");
-                for (i, s) in possible_slots.iter().enumerate() {
-                    println!("{}: {}", i, SlotRef(*s));
-                }
-                if let Some(choice) = get_input() {
-                    ask_about_activity(&mut day, &occupations, now_or_last_entry, SlotRef(possible_slots[choice]));
-                    save_file(&mut day, file.as_path());
-                    ask_about_activity(&mut day, &occupations, SlotRef(possible_slots[choice]), SlotRef::now().next());
-                }
+                ui.split();
             },
-            _ => ask_about_activity_now(&mut day, &occupations)
+            _ => ui.ask_about_activity_now()
         }
     } else {
-        ask_about_activity_now(&mut day, &occupations);
+        ui.ask_about_activity_now();
     }
-    save_file(&mut day, file.as_path());
-}
-
-fn print_day_stats(day: &Day, with_current_time: bool, trim_start: bool) {
-    let first_non_empty = day.first_non_empty();
-    day.slots().for_each(|(s, o)| {
-        if (!with_current_time || *s <= *SlotRef::now()) && (!trim_start || first_non_empty.is_none() || *s >= *first_non_empty.unwrap()) {
-            println!("{}-{} - {}", s, s.next(), if let Some(occ) = o { occ.to_string() } else { "empty".to_string() });
-        }
-    });
-    println!("Hours Productive: {}, Score: {:0.2}", day.hours_productive(), day.score());
-}
-
-fn save_file(day: &mut Day, path: &Path) {
-    fs::write(path, serde_json::to_string(&day).unwrap()).expect("write failed");
-}
-
-fn ask_about_activity(day: &mut Day, occs: &Vec<Occupation>, start: SlotRef, end: SlotRef) {
-    println!("What did you do from {} - {}?", start.to_string().yellow(), end.to_string().yellow());
-    let occ = Occupation::prompt(&occs);
-    if let Some(occ) = occ {
-        for s in *start .. *end {
-            day.time_slots[s] = Some(occ.clone());
-        }
-    }
-}
-
-fn ask_about_activity_now(day: &mut Day, occs: &Vec<Occupation>) {
-    let now = *SlotRef::now();
-    let start = day.now_or_last_entry();
-    let end = now + 1;
-    ask_about_activity(day, occs, start, SlotRef(end));
+    ui.save();
 }
 
 #[test]
@@ -305,7 +326,7 @@ pub fn test_slots() {
 
 #[test]
 pub fn test_get_by_name() {
-    let occupations = Occupation::get_all();
-    assert_eq!(Occupation::get_by_name(&occupations, &*occupations[0].name), Some(occupations[0].clone()));
-    assert_eq!(Occupation::get_by_name(&occupations, "empty"), None);
+    let activities = Activity::get_all();
+    assert_eq!(Activity::get_by_name(&activities, &*activities[0].name), Some(activities[0].clone()));
+    assert_eq!(Activity::get_by_name(&activities, "empty"), None);
 }
